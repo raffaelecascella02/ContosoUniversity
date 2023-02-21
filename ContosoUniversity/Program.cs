7636ab171dba9cee813e837e2cb3b40900d40f3f
rcascella2 <rcascella2@20222028-AIT>
2023-02-21 17:42:16 +0100

    Aggiungere i file di progetto.

---
+++ ContosoUniversity/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoUniversity.Data;
+namespace ContosoUniversity
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{	
+			var builder = WebApplication.CreateBuilder(args);
+            
+			// Added by the scaffolder
+            builder.Services.AddDbContext<SchoolContext>(options =>
+			    options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext") ?? throw new InvalidOperationException("Connection string 'SchoolContext' not found.")));
+
+			// Database Exception Filter
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+
+			using (var scope = app.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+
+				var context = services.GetRequiredService<SchoolContext>();
+				context.Database.EnsureCreated();
+				// DbInitializer.Initialize(context);
+			}
+
+            app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapRazorPages();
+
+			app.Run();
+		}
+	}
+}
